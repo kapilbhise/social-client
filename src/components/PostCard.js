2530884188d4ed4d9052f0ac81eb9e64045862ca
Kapil Bhise <bhisekapil38@gmail.com>
2021-06-19 18:14:50 +0530

    social media client update title and image

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -17,9 +17,9 @@ function PostCard({
     <Card fluid>
       <Card.Content>
         <Image
-          floated="right"
-          size="mini"
-          src="https://react.semantic-ui.com/images/avatar/large/molly.png"
+          floated='right'
+          size='mini'
+          src='https://avatars.githubusercontent.com/u/56427251?s=60&v=4'
         />
         <Card.Header>{username}</Card.Header>
         <Card.Meta as={Link} to={`/posts/${id}`}>
@@ -29,12 +29,12 @@ function PostCard({
       </Card.Content>
       <Card.Content extra>
         <LikeButton user={user} post={{ id, likes, likeCount }} />
-        <MyPopup content="Comment on post">
-          <Button labelPosition="right" as={Link} to={`/posts/${id}`}>
-            <Button color="blue" basic>
-              <Icon name="comments" />
+        <MyPopup content='Comment on post'>
+          <Button labelPosition='right' as={Link} to={`/posts/${id}`}>
+            <Button color='blue' basic>
+              <Icon name='comments' />
             </Button>
-            <Label basic color="blue" pointing="left">
+            <Label basic color='blue' pointing='left'>
               {commentCount}
             </Label>
           </Button>
